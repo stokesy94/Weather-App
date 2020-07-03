1a45b5308f7868a93beae3eca83f541531ad7be4
unknown <mattstokes94@yahoo.co.ukgit config --global user.name Stokesy94>
2020-07-03 17:23:47 +0100

    Updates

---
+++ precache-manifest.c88c07c926058719e6372c5e1123ed18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7cb5f854c5c2a0be29fe1d3f46b7d7",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "209f6a5312351a4fa0ae",
+    "url": "/react-gh-pages/static/js/2.072978ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.072978ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b286ef9325d55be03805",
+    "url": "/react-gh-pages/static/js/main.3b1d696a.chunk.js"
+  },
+  {
+    "revision": "d683756df05cc771368d",
+    "url": "/react-gh-pages/static/js/runtime-main.4365f876.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94048da3c538ff50513bf6343ed922fa.js"
+  "/react-gh-pages/precache-manifest.c88c07c926058719e6372c5e1123ed18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
